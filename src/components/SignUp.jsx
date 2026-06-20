c1c271ec959760a2580505ade086aff43cfaa98d
Nour Asfour <nourasfour81@gmail.com>
2026-06-20 11:48:36 +0300

    all pages without footer

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,36 @@
+import FormInput from "./FormInput";
+import { Link } from "react-router-dom";
+function SignUp() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 px-4">
+      <form className="w-full max-w-md p-8 space-y-6 bg-white rounded-xl shadow-lg">
+        <h1 className="text-2xl font-bold text-center text-gray-900">
+          Create your account
+        </h1>
+        <form className="space-y-5">
+          <FormInput label="Email address" type="email" name="email" required />
+          <FormInput
+            label="Password"
+            type="password"
+            name="password"
+            required
+          />
+
+          <button
+            type="submit"
+            className="w-full py-2 font-semibold bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
+          >
+            Sign Up
+          </button>
+        </form>
+        <p className="text-sm text-center text-gray-600">
+          Already have an account?
+          <Link to="/signin" className="text-blue-500 hover:underline">
+            Sign In
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
+export default SignUp;
